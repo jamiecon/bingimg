5fa7d29af94ae4a889e30b6d632fd264b57caf4e
initialpuppet <31685362+initialpuppet@users.noreply.github.com>
2017-12-14 14:58:19 +0200

    Apply guidlines as-per pylint.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 #! /usr/bin/python
-import urllib, json, os
+"""This module downloads the Bing home page image, converts it to PNG, and
+   saves it in the current directory.
+"""
+import os
+import urllib
+import json
 from subprocess import call
 
-output = urllib.urlopen("https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB").read()
+bing_data_url = "https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-GB"
+
+output = urllib.urlopen(bing_data_url).read()
 jsonobj = json.loads(output)
 imgurl = jsonobj["images"][0]["url"]
 urllib.urlretrieve("https://www.bing.com/" + imgurl, "bing-daily.jpg")
